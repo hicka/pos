461b8b0fd438a8f062f89aaba79670758d2913c6
F. Ahmed <19439535+hicka@users.noreply.github.com>
2021-08-21 17:37:39 +0500

    Add files via upload

---
+++ src/Pos.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Hicka\Pos;
+
+class Pos
+{
+    // Build your next great package.
+}
---
+++ src/PosServiceProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Hicka\Pos;
+
+use Illuminate\Support\ServiceProvider;
+
+class PosServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        /*
+         * Optional methods to load your package assets
+         */
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'pos');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'pos');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/config.php' => config_path('pos.php'),
+            ], 'config');
+
+            // Publishing the views.
+            /*$this->publishes([
+                __DIR__.'/../resources/views' => resource_path('views/vendor/pos'),
+            ], 'views');*/
+
+            // Publishing assets.
+            /*$this->publishes([
+                __DIR__.'/../resources/assets' => public_path('vendor/pos'),
+            ], 'assets');*/
+
+            // Publishing the translation files.
+            /*$this->publishes([
+                __DIR__.'/../resources/lang' => resource_path('lang/vendor/pos'),
+            ], 'lang');*/
+
+            // Registering package commands.
+            // $this->commands([]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'pos');
+
+        // Register the main class to use with the facade
+        $this->app->singleton('pos', function () {
+            return new Pos;
+        });
+    }
+}
